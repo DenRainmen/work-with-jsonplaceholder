e7cd60b7aa6e5b4e429ba6aaf807cea1bd09b95d
Den <potarykindenis@gmail.com>
2022-08-04 04:24:58 +0300

    Добавили компоненту Button а так же описание в README.MD
    В ридми есть ссылка на видео.

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+type ButtonTypeProps = {
+    buttonName: string;
+    callBack: () => void
+    clearPage: () => void
+}
+
+export function Button(props: ButtonTypeProps) {
+
+
+    const onClickButtonHandler = () => {
+        props.callBack()
+    }
+
+    const onClickButtonHandler2 = () => {
+        props.clearPage()
+    }
+
+    return (
+        <div>
+            <button
+                style={{background: "limegreen", borderRadius: "5px"}}
+                onClick={onClickButtonHandler}>
+                {props.buttonName}
+            </button>
+
+            <button
+                style={{background: "tomato", borderRadius: "5px"}}
+                onClick={onClickButtonHandler2}>
+                Clear the Page
+            </button>
+        </div>
+
+    )
+}
